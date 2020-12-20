233c5c84a94aa15b05603d4aa89ebb440953bdfd
Lloyd Barnes <barnes.lloyd86@gmail.com>
2020-12-20 04:59:33 -0600

    updated script.js

--- script.js
+++ script.js
@@ -7,10 +7,19 @@
       //date format MMDDYYYY
       var loadFunction = function(date) {
         //'09/12/2020'
-        var calendarData = [];  //localStorage.getItem(date);
+        var calendarData = []; 
 
         for (var h = 0; h <= 17; h++) {
             calendarData.push(localStorage.getItem(h));        
             }
             return calendarData;
           }
+          var saveFunction = function(hour, hourInfo) {
+            localStorage.setItem(hour, hourInfo);
+          }
+      
+          var dataService = {
+            loadCalendarItem: loadFunction,
+            saveCalendarItem: saveFunction
+          }
+          
